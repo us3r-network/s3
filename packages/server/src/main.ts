2aeab6b097262613227e8cf84d9d157812e19d67
Ubuntu <ubuntu@ip-172-31-23-32.ap-southeast-1.compute.internal>
2023-02-27 08:40:15 +0000

    add

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { Network } from './entities/stream/stream.entity';
 import CeramicSubscriberService from './stream/ceramic.subscriber.service';
-
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -17,34 +17,51 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  app.enableCors();
+  app.use("*", function (req, res, next) {
+    // 解决跨域问题
+    res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Credentials", true);
+    if (req.method == "OPTIONS") {
+      res.send(200);
+    } else {
+      next();
+    }
+  });
 
-  await app.listen(3002);
+
+  app.use("*", createProxyMiddleware({
+    target: 'https://ceramic-private-clay.3boxlabs.com',
+    changeOrigin: true,
+  }));
+
+  await app.listen(3000);
 
   const ceramicSubscriberService = app.get(CeramicSubscriberService);
   // Sub ceramic test network.
-  await ceramicSubscriberService.SubCeramic(
-    Network.TESTNET,
-    [
-      '/dns4/go-ipfs-ceramic-public-clay-external.3boxlabs.com/tcp/4011/ws/p2p/QmWiY3CbNawZjWnHXx3p3DXsg21pZYTj4CRY1iwMkhP8r3',
-      '/dns4/go-ipfs-ceramic-public-clay-external.ceramic.network/tcp/4011/ws/p2p/QmSqeKpCYW89XrHHxtEQEWXmznp6o336jzwvdodbrGeLTk',
-      '/dns4/go-ipfs-ceramic-private-clay-external.3boxlabs.com/tcp/4011/ws/p2p/QmQotCKxiMWt935TyCBFTN23jaivxwrZ3uD58wNxeg5npi',
-      '/dns4/go-ipfs-ceramic-private-cas-clay-external.3boxlabs.com/tcp/4011/ws/p2p/QmbeBTzSccH8xYottaYeyVX8QsKyox1ExfRx7T1iBqRyCd',
-    ],
-    ['/ip4/127.0.0.1/tcp/20001/ws'],
-    '/ceramic/testnet-clay',
-    'https://ceramic-clay.3boxlabs.com',
-  );
-  // Sub ceramic main network.
-  await ceramicSubscriberService.SubCeramic(
-    Network.MAINNET,
-    [
-      '/dns4/go-ipfs-ceramic-private-mainnet-external.3boxlabs.com/tcp/4011/ws/p2p/QmXALVsXZwPWTUbsT8G6VVzzgTJaAWRUD7FWL5f7d5ubAL',
-      '/dns4/go-ipfs-ceramic-private-cas-mainnet-external.3boxlabs.com/tcp/4011/ws/p2p/QmUvEKXuorR7YksrVgA7yKGbfjWHuCRisw2cH9iqRVM9P8',
-    ],
-    ['/ip4/127.0.0.1/tcp/30001/ws'],
-    '/ceramic/mainnet',
-    'https://ceramic.passport-iam.gitcoin.co/',
-  );
+  // await ceramicSubscriberService.SubCeramic(
+  //   Network.TESTNET,
+  //   [
+  //     '/dns4/go-ipfs-ceramic-public-clay-external.3boxlabs.com/tcp/4011/ws/p2p/QmWiY3CbNawZjWnHXx3p3DXsg21pZYTj4CRY1iwMkhP8r3',
+  //     '/dns4/go-ipfs-ceramic-public-clay-external.ceramic.network/tcp/4011/ws/p2p/QmSqeKpCYW89XrHHxtEQEWXmznp6o336jzwvdodbrGeLTk',
+  //     '/dns4/go-ipfs-ceramic-private-clay-external.3boxlabs.com/tcp/4011/ws/p2p/QmQotCKxiMWt935TyCBFTN23jaivxwrZ3uD58wNxeg5npi',
+  //     '/dns4/go-ipfs-ceramic-private-cas-clay-external.3boxlabs.com/tcp/4011/ws/p2p/QmbeBTzSccH8xYottaYeyVX8QsKyox1ExfRx7T1iBqRyCd',
+  //   ],
+  //   ['/ip4/127.0.0.1/tcp/20000/ws'],
+  //   '/ceramic/testnet-clay',
+  //   'https://ceramic-clay.3boxlabs.com',
+  // );
+  // // Sub ceramic main network.
+  // await ceramicSubscriberService.SubCeramic(
+  //   Network.MAINNET,
+  //   [
+  //     '/dns4/go-ipfs-ceramic-private-mainnet-external.3boxlabs.com/tcp/4011/ws/p2p/QmXALVsXZwPWTUbsT8G6VVzzgTJaAWRUD7FWL5f7d5ubAL',
+  //     '/dns4/go-ipfs-ceramic-private-cas-mainnet-external.3boxlabs.com/tcp/4011/ws/p2p/QmUvEKXuorR7YksrVgA7yKGbfjWHuCRisw2cH9iqRVM9P8',
+  //   ],
+  //   ['/ip4/127.0.0.1/tcp/30000/ws'],
+  //   '/ceramic/mainnet',
+  //   'https://ceramic.passport-iam.gitcoin.co/',
+  // );
 }
 bootstrap();
