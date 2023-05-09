320019fa7a0f1f0624730d85dd3fdfcd0b5c7cff
shixuewen <friendlysxw@163.com>
2023-05-09 10:20:48 +0800

    fix: mobile nav useravatar

--- packages/client/src/components/MobileNav.tsx
+++ packages/client/src/components/MobileNav.tsx
@@ -14,10 +14,9 @@ import {
   useAuthentication,
   useSession,
 } from "@us3r-network/auth-with-rainbowkit";
-import { UserAvatar } from "@us3r-network/profile";
+import UserAvatarStyled from "./common/UserAvatarStyled";
 
 export default function Nav() {
-
   const { signIn, signOut } = useAuthentication();
   const session = useSession();
   let location = useLocation();
@@ -49,7 +48,7 @@ export default function Nav() {
 
       <div className="right">
         {session ? (
-          <UserAvatar width={30} height={30} />
+          <UserAvatarStyled />
         ) : (
           <div
             onClick={() => {
@@ -83,9 +82,9 @@ export default function Nav() {
                   </div>
                 </Link>
 
-                <Link to={'/models'} onClick={() => setOpenFilter(!openFilter)}>
-                  <div className={`nav-item ${modelActive ? 'active' : ''}`}>
-                    <ModelIcon stroke={modelActive ? 'white' : '#718096'} />
+                <Link to={"/models"} onClick={() => setOpenFilter(!openFilter)}>
+                  <div className={`nav-item ${modelActive ? "active" : ""}`}>
+                    <ModelIcon stroke={modelActive ? "white" : "#718096"} />
 
                     <div className="tint-c">
                       <div className="tint">ComposeDB Models</div>
