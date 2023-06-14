d76dfe34c8b262d70e05aea51099e6f415f40061
张云鹤 <zhangyunhe1114@gmail.com>
2023-06-14 10:28:52 +0800

    add new features for server

--- packages/server/src/entities/dapp/dapp.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { Dapp } from './dapp.entity';
-
-@EntityRepository(Dapp)
-export class DappRepository extends Repository<Dapp> {}
--- packages/server/src/health/health.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HttpModule } from '@nestjs/axios';
-import { TerminusModule } from '@nestjs/terminus';
-import { HealthController } from './health.controller';
-@Module({
-  controllers: [HealthController],
-  imports: [TerminusModule, HttpModule],
-})
-export class HealthModule {}
