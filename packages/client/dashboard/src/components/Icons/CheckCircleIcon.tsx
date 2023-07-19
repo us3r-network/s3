efd9f326e15e62a95ea7ec1826640fb6d5dbe7b1
shixuewen <friendlysxw@163.com>
2023-07-19 19:23:50 +0800

    feat: dapp home ui

--- packages/client/dashboard/src/components/Icons/CheckCircleIcon.tsx
+++ packages/client/dashboard/src/components/Icons/CheckCircleIcon.tsx
@@ -1,4 +1,4 @@
-export default function CheckCircleIcon() {
+export default function CheckCircleIcon({ bgc = '#5BA85A' }: { bgc?: string }) {
   return (
     <svg
       width="20"
@@ -9,7 +9,7 @@ export default function CheckCircleIcon() {
     >
       <path
         d="M10.0003 18.3327C14.6027 18.3327 18.3337 14.6017 18.3337 9.99935C18.3337 5.39698 14.6027 1.66602 10.0003 1.66602C5.39795 1.66602 1.66699 5.39698 1.66699 9.99935C1.66699 14.6017 5.39795 18.3327 10.0003 18.3327Z"
-        fill="#5BA85A"
+        fill={bgc}
       />
       <path
         d="M6.25 10L8.75 12.5L13.75 7.5"
