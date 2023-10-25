f033c7f408ede50c47c29e9d3157fe9017367a6d
张云鹤 <zhangyunhe1114@gmail.com>
2023-10-25 11:50:52 +0800

    debug ipfs

--- packages/server/src/stream/sync/history-sync.service.ts
+++ packages/server/src/stream/sync/history-sync.service.ts
@@ -40,7 +40,7 @@ export default class HistorySyncService {
         // init ipfs client
         const ipfsHttpClient = await _importDynamic('ipfs-http-client');
         this.ipfs = await ipfsHttpClient.create({
-            url: 'https://gateway.ipfs.io',
+            url: 'https://4everland.io',
         });
     }
 
@@ -130,7 +130,7 @@ export default class HistorySyncService {
         const timeoutMs = 3000;
         const resolution = await this.ipfs.dag.resolve(cid, {
             timeout: timeoutMs,
-            // path: metedataPath,
+            path: metedataPath,
         });
         this.logger.log(`[${cid}] Block resolution: ${JSON.stringify(resolution)}`);
         const blockCid = resolution.cid
