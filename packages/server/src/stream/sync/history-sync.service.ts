ea005633e7498da6450f4119816f5a93847222c6
张云鹤 <zhangyunhe1114@gmail.com>
2023-10-25 13:50:57 +0800

    debug ipfs

--- packages/server/src/stream/sync/history-sync.service.ts
+++ packages/server/src/stream/sync/history-sync.service.ts
@@ -40,7 +40,7 @@ export default class HistorySyncService {
         // init ipfs client
         const ipfsHttpClient = await _importDynamic('ipfs-http-client');
         this.ipfs = await ipfsHttpClient.create({
-            url: 'https://ipfs.io/',
+            url: 'https://gateway.ipfs.io/',
         });
     }
 
