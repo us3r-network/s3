f56475048a56e622c16d5c364ad0d4796030ba87
张云鹤 <zhangyunhe1114@gmail.com>
2023-10-25 14:55:15 +0800

    debug ipfs

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -35,28 +35,28 @@ async function bootstrap() {
     await historySyncService.init(ceramicSubscriberService.jobQueue);
     await historySyncService.startHistorySync();
 
-    // // Subsciber ceramic test network.
-    // await ceramicSubscriberService.subCeramic(
-    //   Network.TESTNET,
-    //   [
-    //     '/dns4/go-ipfs-ceramic-public-clay-external.3boxlabs.com/tcp/4011/ws/p2p/QmWiY3CbNawZjWnHXx3p3DXsg21pZYTj4CRY1iwMkhP8r3',
-    //     '/dns4/go-ipfs-ceramic-private-clay-external.3boxlabs.com/tcp/4011/ws/p2p/QmQotCKxiMWt935TyCBFTN23jaivxwrZ3uD58wNxeg5npi',
-    //     '/dns4/go-ipfs-ceramic-private-cas-clay-external.3boxlabs.com/tcp/4011/ws/p2p/QmbeBTzSccH8xYottaYeyVX8QsKyox1ExfRx7T1iBqRyCd',
-    //   ],
-    //   ['/ip4/127.0.0.1/tcp/20000/ws'],
-    //   '/ceramic/testnet-clay',
-    // );
+    // Subsciber ceramic test network.
+    await ceramicSubscriberService.subCeramic(
+      Network.TESTNET,
+      [
+        '/dns4/go-ipfs-ceramic-public-clay-external.3boxlabs.com/tcp/4011/ws/p2p/QmWiY3CbNawZjWnHXx3p3DXsg21pZYTj4CRY1iwMkhP8r3',
+        '/dns4/go-ipfs-ceramic-private-clay-external.3boxlabs.com/tcp/4011/ws/p2p/QmQotCKxiMWt935TyCBFTN23jaivxwrZ3uD58wNxeg5npi',
+        '/dns4/go-ipfs-ceramic-private-cas-clay-external.3boxlabs.com/tcp/4011/ws/p2p/QmbeBTzSccH8xYottaYeyVX8QsKyox1ExfRx7T1iBqRyCd',
+      ],
+      ['/ip4/127.0.0.1/tcp/20000/ws'],
+      '/ceramic/testnet-clay',
+    );
   
-    // // Subsciber ceramic main network.
-    // await ceramicSubscriberService.subCeramic(
-    //   Network.MAINNET,
-    //   [
-    //     '/dns4/go-ipfs-ceramic-private-mainnet-external.3boxlabs.com/tcp/4011/ws/p2p/QmXALVsXZwPWTUbsT8G6VVzzgTJaAWRUD7FWL5f7d5ubAL',
-    //     '/dns4/go-ipfs-ceramic-private-cas-mainnet-external.3boxlabs.com/tcp/4011/ws/p2p/QmUvEKXuorR7YksrVgA7yKGbfjWHuCRisw2cH9iqRVM9P8',
-    //   ],
-    //   ['/ip4/127.0.0.1/tcp/30000/ws'],
-    //   '/ceramic/mainnet',
-    // );
+    // Subsciber ceramic main network.
+    await ceramicSubscriberService.subCeramic(
+      Network.MAINNET,
+      [
+        '/dns4/go-ipfs-ceramic-private-mainnet-external.3boxlabs.com/tcp/4011/ws/p2p/QmXALVsXZwPWTUbsT8G6VVzzgTJaAWRUD7FWL5f7d5ubAL',
+        '/dns4/go-ipfs-ceramic-private-cas-mainnet-external.3boxlabs.com/tcp/4011/ws/p2p/QmUvEKXuorR7YksrVgA7yKGbfjWHuCRisw2cH9iqRVM9P8',
+      ],
+      ['/ip4/127.0.0.1/tcp/30000/ws'],
+      '/ceramic/mainnet',
+    );
   }
 }
 bootstrap();
