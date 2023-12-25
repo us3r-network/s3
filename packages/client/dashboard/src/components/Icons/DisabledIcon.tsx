4962314bbf82b100cbf4d9fe44e9cd5045a23530
bufan <bufan@hotmail.com>
2023-12-25 16:47:55 +0800

    fix disable icon warning

--- packages/client/dashboard/src/components/Icons/DisabledIcon.tsx
+++ packages/client/dashboard/src/components/Icons/DisabledIcon.tsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: bufan bufan@hotmail.com
+ * @Date: 2023-12-15 10:06:03
+ * @LastEditors: bufan bufan@hotmail.com
+ * @LastEditTime: 2023-12-25 16:45:33
+ * @FilePath: /s3/packages/client/dashboard/src/components/Icons/DisabledIcon.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 export default function DisabledIcon() {
   return (
     <svg
@@ -16,7 +24,7 @@ export default function DisabledIcon() {
         fill="#1B1E23"
         stroke="#718096"
       />
-      <path d="M7 17L17 7" stroke="#718096" stroke-linecap="round" />
+      <path d="M7 17L17 7" stroke="#718096" strokeLinecap="round" />
     </svg>
   )
 }
